675e99d01a8af8bc490d5462c9cabe5bb5f82003
yidong zhang <yidong.zhang@amlogic.com>
2021-08-26 11:18:38 +0800

    subtitle: init subtitle for s-amloigc [1/1]
    PD#SWPL-58389
    
    Problem:
    init subtitle for s-amloigc .
    
    Solution:
    add subtitleserver& jni in subtitle repo.
    
    Verify:
    locally.
    
    Change-Id: I6194d65cecc3dcb636bf80133df723e6e3ce7da3
    Signed-off-by: yidong zhang <yidong.zhang@amlogic.com>

---
+++ subtitleserver/client/subtitleServerHidlClient/Android.mk
@@ -0,0 +1,36 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES:= \
+    SubtitleServerHidlClient.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    vendor.amlogic.hardware.subtitleserver@1.0 \
+    libbase \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libcutils \
+    libutilscallstack \
+    libutils
+
+LOCAL_C_INCLUDES += \
+  system/libhidl/transport/include/hidl \
+  system/libhidl/libhidlmemory/include
+
+LOCAL_C_INCLUDES += \
+   external/libcxx/include
+
+LOCAL_CFLAGS += -std=c++14 -Wno-unused-parameter -Wno-unused-variable
+
+LOCAL_MODULE:= libsubtitlebinder
+LOCAL_LICENSE_KINDS:= SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS:= notice
+
+#ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+#LOCAL_PROPRIETARY_MODULE := true
+#endif
+
+#include $(BUILD_SHARED_LIBRARY)
